64f20a278666c609b57d81a825a8d083f0b73ae0
joaquindiaz21 <joaquindiaz2103@gmail.com>
2025-06-22 17:16:46 -0300

    cambios

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
 
-const Header = () => {
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import logovet from "../assets/logovet.svg";
+import login from "../assets/login.svg"
+
+function Header() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Navbar className="bg-body-tertiary ">
+      <div className="px-5 w-100 d-flex align-items-center justify-content-between">
+        <Navbar.Brand>
+          <img
+            src={logovet}
+            width="200px"
+            
+            className="d-inline-block align-top"
+            alt="Logo Veterinaria"
+          />
+        </Navbar.Brand>
+
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <a href="/login"><img
+            src={login}
+            width="50px"
+            alt="Login"
+          /> </a>
+        </Navbar.Collapse>
+      </div>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
