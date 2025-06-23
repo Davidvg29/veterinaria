c6d1dab3820b914a57a06cc291b27aa96b50b8f4
joaquindiaz21 <joaquindiaz2103@gmail.com>
2025-06-23 16:01:32 -0300

    agregue una imagen mas al carrusel

--- frontend/src/Components/Carrousel.jsx
+++ frontend/src/Components/Carrousel.jsx
@@ -2,6 +2,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import logo1 from '../assets/logo1.svg';
 import logo2 from '../assets/logo2.svg';
 import logo3 from '../assets/logo3.svg';
+import logo4 from '../assets/logo4.svg';
  
 function Carrusel() {
   return (
@@ -17,7 +18,7 @@ function Carrusel() {
       <Carousel.Item>
         <img
       className="d-block w-100"
-      src={logo2}
+      src={logo4}
       alt="First slide"
       style={{ height: "650px", objectFit: "cover" }}
         />
@@ -26,6 +27,14 @@ function Carrusel() {
         <img
       className="d-block w-100"
       src={logo3}
+      alt="First slide"
+      style={{ height: "650px", objectFit: "cover" }}
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+      className="d-block w-100"
+      src={logo2}
       alt="First slide"
       style={{ height: "650px", objectFit: "cover" }}
         />
