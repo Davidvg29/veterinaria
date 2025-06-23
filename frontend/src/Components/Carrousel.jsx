d45b2e6469d5f2a507cc73041d969fb760597219
joaquindiaz21 <joaquindiaz2103@gmail.com>
2025-06-23 15:29:57 -0300

    le cambie el tamaño al carrusel

--- frontend/src/Components/Carrousel.jsx
+++ frontend/src/Components/Carrousel.jsx
@@ -2,7 +2,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import logo1 from '../assets/logo1.svg';
 import logo2 from '../assets/logo2.svg';
 import logo3 from '../assets/logo3.svg';
-
+ 
 function Carrusel() {
   return (
     <Carousel interval={3000}>
@@ -11,6 +11,7 @@ function Carrusel() {
     className="d-block w-100"
     src={logo1}
     alt="First slide"
+    style={{ height: "650px", objectFit: "cover" }}
         />
       </Carousel.Item>
       <Carousel.Item>
@@ -18,6 +19,7 @@ function Carrusel() {
       className="d-block w-100"
       src={logo2}
       alt="First slide"
+      style={{ height: "650px", objectFit: "cover" }}
         />
       </Carousel.Item>
       <Carousel.Item>
@@ -25,6 +27,7 @@ function Carrusel() {
       className="d-block w-100"
       src={logo3}
       alt="First slide"
+      style={{ height: "650px", objectFit: "cover" }}
         />
       </Carousel.Item>
     </Carousel>
