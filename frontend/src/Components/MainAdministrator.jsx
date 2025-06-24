c87be25908bfd23759ff3276ae3893fa0b4f7186
Ángel Pastrana <Angel Pastranaangelpastrana0333@gmail.com>
2025-06-24 15:08:43 -0300

    agrego el componente MainAdministrator donde van las cards de cliente y producto ademas agrega la funcionalidad de los botones crear en ambas cards este boton muestra los formularios mediante una ventana modal

---
+++ frontend/src/Components/MainAdministrator.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+
+
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/esm/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import cardCliente from '../assets/cardCliente.png'
+import cardProducto from '../assets/cardProducto.png'
+import FormClient from './CRUD/clientes/FormClient';
+import FromProducto from './CRUD/productos/FormProduct'
+
+const MainAdministrator = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [fromType, setFromType] = useState("");
+
+    const handleOpenModal = (type) => {
+        setFromType(type);
+        setShowModal(true);
+    };
+
+    const handleCloseModal = (type) => {
+        setShowModal(false);
+        setFromType("");
+    };
+
+    return (
+        <>
+            <div className='d-flex flex-wrap  justify-content-center align-items-center'>
+                <Card style={{ width: '18rem' }} className="mx-3 my-2">
+                    <Card.Img variant="top" src={cardCliente} />
+                    <Card.Body>
+                        <Card.Title>Clientes</Card.Title>
+                        <Card.Text>
+                            Administrá todos tus clientes desde aquí.
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="d-flex justify-content-between flex-wrap gap-2 p-2">
+                        <Button size="sm" variant="success" onClick={() => handleOpenModal("cliente")} >Crear</Button>
+                        <Button size="sm" variant="primary" >Ver</Button>
+                        <Button size="sm" variant="warning" >Editar</Button>
+                        <Button size="sm" variant="danger" >Eliminar</Button>
+                    </ListGroup>
+                </Card>
+
+                <Card style={{ width: '18rem' }} className="mx-3 my-2">
+                    <Card.Img variant="top" src={cardProducto} />
+                    <Card.Body>
+                        <Card.Title>Productos</Card.Title>
+                        <Card.Text>
+                            Administrá todos tus productos desde aquí
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="d-flex justify-content-between flex-wrap gap-2 p-2">
+                        <Button size="sm" variant="success" onClick={() => handleOpenModal("producto")} >Crear</Button>
+                        <Button size="sm" variant="primary" >Ver</Button>
+                        <Button size="sm" variant="warning" >Editar</Button>
+                        <Button size="sm" variant="danger" >Eliminar</Button>
+                    </ListGroup>
+                </Card>
+            </div>
+            <Modal show={showModal} onHide={handleCloseModal} size='lg' scrollable centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>{fromType==="cliente"? "Nuevo Cliente" : "Nuevo Producto"}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="d-flex justify-content-center ">
+                    {fromType === "cliente" && <FormClient/>}
+                    {fromType === "producto" && <FromProducto/>}
+                </Modal.Body>
+
+
+            </Modal>
+        </>
+    )
+}
+
+export default MainAdministrator
--- frontend/src/Pages/Administrator.jsx
+++ frontend/src/Pages/Administrator.jsx
@@ -1,11 +1,14 @@
 import FormClient from "../Components/CRUD/clientes/FormClient"
 import FormProduct from "../Components/CRUD/productos/FormProduct"
-
+import MainAdministrator from "../Components/MainAdministrator"
+import Header from "../Components/Header"
+import Footer from "../Components/Footer"
 const Administrator = ()=>{
     return(
         <>
-         <FormClient/>
-         <FormProduct/>
+         <Header/>
+         <MainAdministrator/>
+         <Footer/>
         </>
     )
 }
