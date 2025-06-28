9ad3dc99e18198c721c40555ada0023eab0e9996
Ángel Pastrana <Angel Pastranaangelpastrana0333@gmail.com>
2025-06-28 03:42:48 -0300

    agrego el header y el footer a la page Productos

--- frontend/src/Pages/Productos.jsx
+++ frontend/src/Pages/Productos.jsx
@@ -1,10 +1,14 @@
 import MainProduct from "../Components/CRUD/productos/MainProducto";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
 
 
 const Productos = () => {
     return ( 
-        <>
+        <>  
+            <Header/>
             <MainProduct/>
+            <Footer/>
         </>
      );
 }
